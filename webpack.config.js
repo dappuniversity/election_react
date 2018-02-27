45f136d125d041019bfc970a510c1ef1ea788276
Gregory McCubbin <gwmccubbin@gmail.com>
2018-02-27 12:18:02 -0600

    Adds webpack & webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+module.exports = {
+  entry: path.join(__dirname, 'src/js', 'index.js'),
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ],
+        include: /src/
+      }, {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+            presets: ['es2015', 'react', 'stage-2']
+        }
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  }
+}
