47d8bdd8e1bba39ff929afb31812dc5ce5af846c
Gregory McCubbin <gwmccubbin@gmail.com>
2018-02-27 16:07:46 -0600

    WIP renders react app component

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Web3 from 'web3'
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <h1>Election Results</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+   <App />,
+   document.querySelector('#root')
+)
